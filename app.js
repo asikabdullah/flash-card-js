c9e52a2079cb15c7f72a00675dd6fce63bc2d862
asikabdullah <66024244+asikabdullah@users.noreply.github.com>
2022-07-16 10:33:03 +0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const term = document.querySelector('.term');
-const nextButton = document.querySelector('.check');
-
-checkButton.addEventListener('click', function() {
-    console.log("You click the check button");
-})
